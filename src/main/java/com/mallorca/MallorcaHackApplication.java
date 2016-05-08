c88948733b0406c62baec42c8678dd407a4bdfb3
PmiElita <nazar@hembara.com>
2016-05-08 07:16:17 +0200

    added transport service

--- src/main/java/com/mallorca/MallorcaHackApplication.java
+++ src/main/java/com/mallorca/MallorcaHackApplication.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import com.hotelbeds.distribution.hotel_api_sdk.types.HotelApiSDKException;
 import com.hotelbeds.hotelapimodel.auto.model.Hotel;
@@ -13,11 +14,19 @@ import com.hotelbeds.tab.sdk.client.base.exception.HttpClientException;
 import com.hotelbeds.tab.sdk.model.pojo.PurchasableActivity;
 import com.mallorca.service.ActivityService;
 import com.mallorca.service.HotelService;
+import com.mallorca.service.TransportService;
 
 @SpringBootApplication
 public class MallorcaHackApplication {
 
 	public static void main(String[] args) throws HotelApiSDKException, HttpClientException, IOException {
+		//start main application
+		ApplicationContext appContext = SpringApplication.run(MallorcaHackApplication.class, args);
+		
+		TransportService transportService = appContext.getBean(TransportService.class);
+		System.out.println(transportService.getMinPrice("Berlin", "Palma") + " EUR "+transportService.getRome2RioLink("Berlin", "Palma"));
+		
+		
 		//Activity API
 		String activityCode = "E-U10-SEAWRLDSDG";
 		ActivityService activityService = new ActivityService();
@@ -31,7 +40,6 @@ public class MallorcaHackApplication {
 		Hotel hotel = hotelService.findCheapesHotelInRadius(LocalDate.now().plusDays(20) , LocalDate.now().plusDays(30), "39.694967", "3.017201");
 		System.out.println(hotel.getMinRate() + hotel.getCurrency() + " " + hotel.getName() +" in "+ hotel.getDestinationName());
 		
-		//start main application
-		SpringApplication.run(MallorcaHackApplication.class, args);
+		
 	}
 }
